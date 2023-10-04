ca7a46e496c8920a0d5d7c20ec3f3147cecec0a4
Jana Salah <janaabuhaltam@gmail.com>
2023-10-04 08:09:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+const record = [
+    { year: "2018", result: "L" },
+    { year: "2017", result: "W" },
+    { year: "2016", result: "N/A" },
+    //...
+  ];
+
+  
+function superbowlWin(arr) {
+    let win = arr.find((element) => element.result === "W");
+    return win ? win.year : undefined;
+    
+}
